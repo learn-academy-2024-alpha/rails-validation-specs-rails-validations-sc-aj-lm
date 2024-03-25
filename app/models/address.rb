f8b329493a487581ddd17cca1de68e5aab64d7d2
Louie-cpu <luis.u.m779@gmail.com>
2024-03-25 15:01:13 -0700

    stretch goals

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,2 +1,4 @@
 class Address < ApplicationRecord
+    belongs_to :Account, optional: true
+    validates :street_name, :street_number, :city, :state, :zip
 end
--- spec/models/address_spec.rb
+++ spec/models/address_spec.rb
@@ -1,5 +1,49 @@
 require 'rails_helper'
 
 RSpec.describe Address, type: :model do
-belongs_to :Account, optional: true
+  it 'is not valid without a street_number' do
+    Little = Account.create(
+      street_name:'elm st',
+      city: 'omaha',
+      state:'nebraska',
+      zip: '12'
+      )
+      expect(Little.errors[:street_number]).to_not be_empty
+  end
+  it 'is not valid without a street_name' do
+    Little = Account.create(
+      street_number:'999',
+      city: 'omaha',
+      state:'nebraska',
+      zip: '12'
+      )
+      expect(Little.errors[:street_name]).to_not be_empty
+  end
+  it 'is not valid without a city' do
+    Little = Account.create(
+      street_number:'999',
+      street_name:'elm st',
+      state:'nebraska',
+      zip: '12'
+      )
+      expect(Little.errors[:state]).to_not be_empty
+  end
+  it 'is not valid without a city' do
+    Little = Account.create(
+      street_number:'999',
+      street_name:'elm st',
+      city:'omaha',
+      zip: '12'
+      )
+      expect(Little.errors[:state]).to_not be_empty
+  end
+  it 'is not valid without a zip' do
+    Little = Account.create(
+      street_number:'999',
+      street_name:'elm st',
+      state:'nebraska',
+      city: 'omaha'
+      )
+      expect(Little.errors[:zip]).to_not be_empty
+  end
 end
